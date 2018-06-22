0fdd16c94433ae2e0e6bcca2a9e2797a15702802
ceboulba <antoine@archipicture.fr>
2018-06-23 00:52:31 +0200

    for

--- exo-04-whatIsInAName.js
+++ exo-04-whatIsInAName.js
@@ -3,6 +3,13 @@ function whatIsInAName(collection, source) {
   var arr = [];
   // Only change code below this line
 
+  const k = Object.keys(source);
+  console.log('k: ',k[0])
+
+collection.forEach(element => {
+  console.log('element: ',element[k[0]]);
+});
+
   // Only change code above this line
   return arr;
 }
