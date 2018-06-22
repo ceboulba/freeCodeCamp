9715f5830f19879907c8b700d86e270e6c5dcbc4
ceboulba <antoine@archipicture.fr>
2018-06-23 00:24:15 +0200

    for

--- exo-03-destroyer.js
+++ exo-03-destroyer.js
@@ -3,18 +3,13 @@ function destroyer(arr) {
   console.log('tab01:',tab01);
   console.log('tab02:',tab02);
 
-tab02.forEach(x=>tab01.includes(x) ? console.log('oui pour:',x) : console.log('non pour:',x ) )
+  const r = [];
 
+tab02.forEach(x=>tab01.includes(x) ? console.log('oui pour:',x) : r.push(x));
+tab01.forEach(x=>tab02.includes(x) ? console.log('oui pour:',x) : r.push(x));
 
-
-  // Remove all the values
-
-  // let [arr01, ...arr02] = [arr]
-  // console.log('arr01:',arr01);
-  // console.log('arr02:',arr02);
-
-
-  // return arr;
+console.log('r: ',r)
+ return r;
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
