b6d9d2d3fb217e2ca8ddb647d9ffb33431f8dffa
ceboulba <antoine@archipicture.fr>
2018-06-27 02:12:50 +0200

    five

--- CHALLENGE - cashRegister.js
+++ CHALLENGE - cashRegister.js
@@ -1,28 +1,51 @@
 function checkCashRegister(price, cash, cid) {
+  const monnaie = cash - price;
+  console.log(`monnaie à rendre: ${monnaie}`);
   var change;
+  console.log(`price= ${price}, cash= ${cash} and cid=${cid}`);
+  console.log('typeOF: ', typeof(cid));
+  console.log('CID: ', cid[0]);
+  const maCaisse = {};
+  cid.map(obj => {
+    const name = obj[0];
+    const valor = obj[1];
+    maCaisse[name] = obj[1]
+  })
+
+console.log('MA CAISSE PENNY:', maCaisse)
+ 
+
+  function test(somme) {
+
+  }
+
   // Here is your change, ma'am.
   return change;
 }
 
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.1],
-// ["QUARTER", 4.25],
-// ["ONE", 90],
-// ["FIVE", 55],
-// ["TEN", 20],
-// ["TWENTY", 60],
-// ["ONE HUNDRED", 100]]
-
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
-
-
-
-
-
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+]);
 
 /*
+$0.01 (PENNY)
+$0.05 (NICKEL)
+$0.1  (DIME)
+$0.25 (QUARTER)
+$1    (DOLLAR)
+$5    (FIVE)
+$10   (TEN)
+$20   (TWENTY)
+$100  (ONE HUNDRED)
+
 JavaScript Algorithms and Data Structures Projects: Cash Register
 Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
 
@@ -35,4 +58,5 @@ Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than
 Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
 
 Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+
 */
