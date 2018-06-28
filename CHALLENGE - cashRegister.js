3293d2269eb896a21ba74e730ca95111b9f1ea38
ceboulba <antoine@archipicture.fr>
2018-06-28 11:33:08 +0200

    qsdf

--- CHALLENGE - cashRegister.js
+++ CHALLENGE - cashRegister.js
@@ -8,7 +8,8 @@ function checkCashRegister(price, cash, cid) {
 }
 
 // Example cash-in-drawer array:
-// [["PENNY", 1.01],
+// [
+// ["PENNY", 1.01],
 // ["NICKEL", 2.05],
 // ["DIME", 3.1],
 // ["QUARTER", 4.25],
@@ -16,7 +17,8 @@ function checkCashRegister(price, cash, cid) {
 // ["FIVE", 55],
 // ["TEN", 20],
 // ["TWENTY", 60],
-// ["ONE HUNDRED", 100]]
+// ["ONE HUNDRED", 100]
+// ]
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
