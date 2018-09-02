a135c7f091ac3031ab19fcecc66fd5e2ac22941d
ceboulba <antoine@archipicture.fr>
2018-09-02 23:17:04 +0200

    02-09-2018 23:15

--- CHALLENGE - cashRegister.js
+++ CHALLENGE - cashRegister.js
@@ -1,87 +1,93 @@
 function checkCashRegister(price, cash, cid) {
   // preparation de l'objet à rendre
   const reponse = {
-    status: '',
+    status: "",
     change: []
-  }
+  };
 
   //  tableau des valeurs //
   const valors = [
-    ['ONE HUNDRED', 100],
-    ['TWENTY', 20],
-    ['TEN', 10],
-    ['FIVE', 5],
-    ['ONE', 1],
-    ['QUARTER', 0.25],
-    ['DIME', 0.1],
-    ['NICKEL', 0.05],
-    ['PENNY', 0.01]
-  ]
+    ["ONE HUNDRED", 100],
+    ["TWENTY", 20],
+    ["TEN", 10],
+    ["FIVE", 5],
+    ["ONE", 1],
+    ["QUARTER", 0.25],
+    ["DIME", 0.1],
+    ["NICKEL", 0.05],
+    ["PENNY", 0.01]
+  ];
 
-  const cashInDrawer = cid.reverse()
+  const cashInDrawer = cid.reverse();
 
   // calcul de la somme a rendre de base  //
-  let sommeARendre = cash - price
+  let sommeARendre = cash - price;
 
   // totalEnCaisse
-  const totalEnCaisse = (arr) => arr.reduce((acc, curr) => acc + curr[1], 0)
+  const totalEnCaisse = arr => arr.reduce((acc, curr) => acc + curr[1], 0);
 
   const choisir = () => {
     return totalEnCaisse(cid) === sommeARendre
       ? egal()
       : totalEnCaisse(cid) < sommeARendre
         ? pasAssez()
-        : rendre()
-  }
+        : rendre();
+  };
 
   function renderPossible() {
-    const valide = totalEnCaisse(cashInDrawer.filter(valor => valors.filter(cidValor => cidValor[0] === valor[0] && valor[1] <= sommeARendre)))
-   return valide > sommeARendre
-   ? true : false
+    const valide = valors.filter(valor => valor[1] <= sommeARendre);
+    //console.log('valide => ', valide)
+    const arr = [];
+    cashInDrawer.forEach(val =>
+      valide.forEach(valor => {
+        // console.log(`val => ${val} valor => ${valor}`);
+        if (val[0] === valor[0] && val[1] > 0) {
+          arr.push(val)
+        }
+      })
+    );
+    //console.log("arr => ", arr);
+    return arr
   }
-  console.log('renderPossible => ', renderPossible())
+  const good = renderPossible();
+  console.log('good => ', good)
+
+  // function renderPossible() {
+  //   const valide = totalEnCaisse(cashInDrawer.filter(valor => valors.filter(cidValor => cidValor[0] === valor[0] && valor[1] <= sommeARendre)))
+  //   console.log('valide => ', valide)
+  //  return valide > sommeARendre
+  //  ? true : false
+  // }
+  // console.log('renderPossible => ', renderPossible())
 
   function rendre() {
-    console.log('inRendre')
+    console.log("inRendre");
   }
 
-  choisir()
+  //choisir()
 
   function videCaisse(valor, cidValor) {
-    console.log('inVideCaisse')
+    console.log("inVideCaisse");
   }
 
   function pasAssez() {
-    reponse.status = 'INSUFFICIENT_FUNDS'
-    reponse.change = []
-    return reponse
+    reponse.status = "INSUFFICIENT_FUNDS";
+    reponse.change = [];
+    return reponse;
   }
 
   function egal() {
-    reponse.status = 'CLOSED'
-    reponse.change.push(...cid.reverse())
-    return reponse
+    reponse.status = "CLOSED";
+    reponse.change.push(...cid.reverse());
+    return reponse;
   }
 
-  console.log('​checkCashRegister -> reponse -> ', reponse)
-  return reponse
+  // console.log('​checkCashRegister -> reponse -> ', reponse)
+  return reponse;
 }
 
 // ///////////////////  APPEL DE L'APP  /////////////////////
-// checkCashRegister(19.5, 20, [
-//   ['PENNY', 1.01],
-//   ['NICKEL', 2.05],
-//   ['DIME', 3.1],
-//   ['QUARTER', 4.25],
-//   ['ONE', 90],
-//   ['FIVE', 55],
-//   ['TEN', 20],
-//   ['TWENTY', 60],
-//   ['ONE HUNDRED', 100]
-// ])
-
-// ///////////////////  APPEL DE L'APP  /////////////////////
-checkCashRegister(3.26, 100, [
+checkCashRegister(19.5, 20, [
   ['PENNY', 1.01],
   ['NICKEL', 2.05],
   ['DIME', 3.1],
@@ -94,10 +100,43 @@ checkCashRegister(3.26, 100, [
 ])
 
 // ///////////////////  APPEL DE L'APP  /////////////////////
-// checkCashRegister(19.5, 20, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]])
+// checkCashRegister(3.26, 100, [
+//   ['PENNY', 1.01],
+//   ['NICKEL', 2.05],
+//   ['DIME', 3.1],
+//   ['QUARTER', 4.25],
+//   ['ONE', 90],
+//   ['FIVE', 55],
+//   ['TEN', 20],
+//   ['TWENTY', 60],
+//   ['ONE HUNDRED', 100]
+// ])
 
 // ///////////////////  APPEL DE L'APP  /////////////////////
-// checkCashRegister(19.5, 20, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 1], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]])
+// checkCashRegister(19.5, 20, [
+//   ["PENNY", 0.01],
+//   ["NICKEL", 0],
+//   ["DIME", 0],
+//   ["QUARTER", 0],
+//   ["ONE", 0],
+//   ["FIVE", 0],
+//   ["TEN", 0],
+//   ["TWENTY", 0],
+//   ["ONE HUNDRED", 0]
+// ]);
+
+// ///////////////////  APPEL DE L'APP  /////////////////////
+checkCashRegister(19.5, 20, [
+  ["PENNY", 0.01],
+  ["NICKEL", 0],
+  ["DIME", 0],
+  ["QUARTER", 0],
+  ["ONE", 1],
+  ["FIVE", 0],
+  ["TEN", 0],
+  ["TWENTY", 0],
+  ["ONE HUNDRED", 0]
+]);
 
 // ///////////////////  APPEL DE L'APP  /////////////////////
 // checkCashRegister(19.5, 20, [['PENNY', 0.5], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]])
