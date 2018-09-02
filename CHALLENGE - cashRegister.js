554767b5f32e0417f67b7a5ebc102b7959baf900
ceboulba <antoine@archipicture.fr>
2018-09-02 09:09:58 +0200

    02-09-2018 09:10

--- CHALLENGE - cashRegister.js
+++ CHALLENGE - cashRegister.js
@@ -1,4 +1,4 @@
-function checkCashRegister (price, cash, cid) {
+function checkCashRegister(price, cash, cid) {
   // preparation de l'objet à rendre
   const reponse = {
     status: '',
@@ -22,59 +22,69 @@ function checkCashRegister (price, cash, cid) {
 
   // calcul de la somme a rendre de base  //
   let sommeARendre = cash - price
+  //console.log('TCL: checkCashRegister -> sommeARendre', sommeARendre);
 
   // totalEnCaisse
-  const totalEnCaisse = cid.reduce((acc, curr) => acc + curr[1], 0).toFixed(2)
+  const totalEnCaisse = cid.reduce((acc, curr) => acc + curr[1], 0)
+  //console.log('TCL: checkCashRegister -> totalEnCaisse', totalEnCaisse);
 
   const choisir = () => {
-    return totalEnCaisse < sommeARendre
-      ? pasAssez()
-      : valors.find(valor => cashInDrawer.find(cidValor => valor[0] === cidValor[0] && valor[1] <= sommeARendre && cidValor[1] > 0))
+    return totalEnCaisse === sommeARendre
+      ? egal()
+      : totalEnCaisse < sommeARendre
+        ? pasAssez()
+        : valors.find(valor => cashInDrawer.find(cidValor => valor[0] === cidValor[0] && valor[1] <= sommeARendre && cidValor[1] > 0))
   }
 
-  console.log('choix => ', choisir())
+  //console.log('choix => ', choisir())
 
-  const rendre = () => {
-    console.log(`on va rendre ${sommeARendre}`)
-    const choix = choisir()
-    let rendu = 0
-    // console.log('rendre => choisir => ', choisir() !== undefined ? 'oui' : pasAssez())
-    const [devise, monnaie] = choisir()
-    let [, stock] = cid.find(val => val[0] === devise)
-    const index = cid.findIndex(element => element[0] === devise)
-    // console.log('rendre -> devise -> ', devise)
-    // console.log('rendre -> monnaie -> ', monnaie)
-    // console.log('rendre -> stock -> ', stock)
-    // console.log('rendre -> stock -> index ', index)
-
-    while (sommeARendre > 0 && stock > 0 && sommeARendre >= monnaie) {
-      sommeARendre -= monnaie
-      stock -= monnaie
-      rendu += monnaie
-      cid[index][1] -= monnaie
-      sommeARendre = sommeARendre.toFixed(2)
-    }
-
-    reponse.status = 'OPEN'
-    reponse.change.push([devise, rendu])
-    // console.log(`
-    //     on va rendre ${sommeARendre}
-    //     et on vient de rendre ${rendu}`)
-
-    if (sommeARendre > 0) {
-      rendre()
-    }
+  function rendre() {
+    choisir()
   }
+
+  rendre()
+
+  // const rendre = () => {
+  //   console.log(`on va rendre ${sommeARendre}`)
+  //   const choix = choisir()
+  //   let rendu = 0
+  //   // console.log('rendre => choisir => ', choisir() !== undefined ? 'oui' : pasAssez())
+  //   const [devise, monnaie] = choisir()
+  //   let [, stock] = cid.find(val => val[0] === devise)
+  //   const index = cid.findIndex(element => element[0] === devise)
+  //   // console.log('rendre -> devise -> ', devise)
+  //   // console.log('rendre -> monnaie -> ', monnaie)
+  //   // console.log('rendre -> stock -> ', stock)
+  //   // console.log('rendre -> stock -> index ', index)
+
+  //   while (sommeARendre > 0 && stock > 0 && sommeARendre >= monnaie) {
+  //     sommeARendre -= monnaie
+  //     stock -= monnaie
+  //     rendu += monnaie
+  //     cid[index][1] -= monnaie
+  //     sommeARendre = sommeARendre.toFixed(2)
+  //   }
+
+  //   reponse.status = 'OPEN'
+  //   reponse.change.push([devise, rendu])
+  //   // console.log(`
+  //   //     on va rendre ${sommeARendre}
+  //   //     et on vient de rendre ${rendu}`)
+
+  //   if (sommeARendre > 0) {
+  //     rendre()
+  //   }
+  // }
 
   // calcul si la somme total en caisse est suffisante //
-  function checkMyTotal() {
-    const totalEnCaisse = cid.reduce((acc, val) => acc + val[1], 0).toFixed(2)
-    console.log('totalEnCaisse -> ', totalEnCaisse)
-    console.log('sommeARendre -> ', sommeARendre)
-    return totalEnCaisse >= sommeARendre
-      ? rendre()
-      : pasAssez()
-  }
+  // function checkMyTotal() {
+  //   const totalEnCaisse = cid.reduce((acc, val) => acc + val[1], 0).toFixed(2)
+  //   console.log('totalEnCaisse -> ', totalEnCaisse)
+  //   console.log('sommeARendre -> ', sommeARendre)
+  //   return totalEnCaisse >= sommeARendre
+  //     ? rendre()
+  //     : pasAssez()
+  // }
 
   // function checkMyTotal () {
   //   const totalEnCaisse = cid.reduce((acc, val) => acc + val[1], 0).toFixed(2)
@@ -85,9 +95,15 @@ function checkCashRegister (price, cash, cid) {
   //     : pasAssez()
   // }
 
-  function pasAssez() {
+  function pasAssez () {
     reponse.status = 'INSUFFICIENT_FUNDS'
     reponse.change = []
+    return reponse
+  }
+
+  function egal () {
+    reponse.status = 'CLOSED'
+    reponse.change.push(...cid.reverse())
     return reponse
   }
 
@@ -106,9 +122,7 @@ function checkCashRegister (price, cash, cid) {
 
   //checkMyTotal()
 
-  console.log('​------------------------')
   console.log('​checkCashRegister -> reponse -> ', reponse)
-  console.log('​------------------------')
   return reponse
 }
 
@@ -139,10 +153,13 @@ checkCashRegister(19.5, 20, [
 // ])
 
 // ///////////////////  APPEL DE L'APP  /////////////////////
-// checkCashRegister(19.5, 20, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]])
+checkCashRegister(19.5, 20, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]])
 
 // ///////////////////  APPEL DE L'APP  /////////////////////
-// checkCashRegister(19.5, 20, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 1], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]])
+checkCashRegister(19.5, 20, [['PENNY', 0.01], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 1], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]])
+
+// ///////////////////  APPEL DE L'APP  /////////////////////
+checkCashRegister(19.5, 20, [['PENNY', 0.5], ['NICKEL', 0], ['DIME', 0], ['QUARTER', 0], ['ONE', 0], ['FIVE', 0], ['TEN', 0], ['TWENTY', 0], ['ONE HUNDRED', 0]])
 
 // NSTRUCTIONS :
 
