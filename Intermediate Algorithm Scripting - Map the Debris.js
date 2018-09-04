1809eeb4522dba29b19bec52336c30381f68894b
ceboulba <antoine@archipicture.fr>
2018-09-04 18:26:25 +0200

    04-09-2018 18:30

--- Intermediate Algorithm Scripting - Map the Debris.js
+++ Intermediate Algorithm Scripting - Map the Debris.js
@@ -1,7 +1,19 @@
-function orbitalPeriod(arr) {
-  var GM = 398600.4418;
-  var earthRadius = 6367.4447;
-  return arr;
+function orbitalPeriod (arr) {
+  var GM = 398600.4418
+  var earthRadius = 6367.4447
+
+
+
+  function calculate(GM, earthRadius, avgAlt, name) {
+    this.GM = Math.floor(GM)
+    this.earthRadius = Math.floor(earthRadius)
+    this.avgAlt = Math.floor(avgAlt)
+    this.name = name
+
+    const orbit = 
+  }
+
+  return arr
 }
 
-orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+orbitalPeriod([{ name: 'sputnik', avgAlt: 35873.5553 }])
