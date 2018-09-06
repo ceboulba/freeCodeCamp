36e53ed754a57b03bb2b8d38dc95edbb945d7fed
ceboulba <antoine@archipicture.fr>
2018-09-06 09:06:16 +0200

    a

--- splice&slice.js
+++ splice&slice.js
@@ -1,15 +1,14 @@
-function frankenSplice (arr1, arr2, n) {
+function frankenSplice(arr1, arr2, n) {
   // It's alive. It's alive!
-  arr2.splice(n, 0, ...arr1)
+  arr2.splice(n, 0, ...arr1);
   // arr2.join().split(',')
-  console.log('arr2', arr2)
-
+  console.log("arr2", arr2);
   // return arr2;
 }
 
-frankenSplice([1, 2, 3], [4, 5], 1)
-frankenSplice([1, 2], ['a', 'b'], 1)
-frankenSplice(['claw', 'tentacle'], ['head', 'shoulders', 'knees', 'toes'], 2)
+frankenSplice([1, 2, 3], [4, 5], 1);
+frankenSplice([1, 2], ["a", "b"], 1);
+frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2);
 
 // running test
 // frankenSplice([1, 2, 3], [4, 5], 1) should return [4, 1, 2, 3, 5].
